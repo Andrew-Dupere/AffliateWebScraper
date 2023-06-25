c63e7dc055a1091a3ebd5c6a39ff48fc48129e21
andrewdupere <andrewdupere@outlook.com>
2023-06-25 12:05:16 -0400

    finished base scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,6 +2,16 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.support.wait import WebDriverWait
+
+import pandas as pd
+
+import re
+import requests
+import bs4
+from bs4 import BeautifulSoup
+
 import time
 
 from datetime import date
@@ -12,15 +22,12 @@ path = r'C:\Users\Andre\Downloads\chromedriver_win32\chromedriver.exe'
 #establish driver for chrome
 driver = webdriver.Chrome(executable_path=path)
 
-
-
 #define site to scrape
 site = 'https://academy.usconcealedcarry.com/search/instructors'
 
-
-
 #main script
 location = 'Austin'
+
 driver.get(site)
 
 time.sleep(1.5)
@@ -48,10 +55,24 @@ showMore.click()
 #grab each individual instructor's url and save to an object
 coachUrls = driver.find_elements(By.CLASS_NAME,'InstructorCard_wrap__arGO7  ')
 
-coaches = []
+coaches = {}
+
 for url in coachUrls:
-   coaches.append(url.get_attribute('href'))
+    coaches[url.find_element(By.TAG_NAME,'h3').text] = url.get_attribute('href')
+    
 
+    
+for name, url in coaches.items():
 
-print(len(coaches))
+    driver.get(url)
+    sr = WebDriverWait(driver,timeout=5).until(lambda d:d.find_element(By.CLASS_NAME,"bp3-card"))
+
+    contact = sr.text.split('\n')
+    coaches[name] = contact
+
+df = pd.DataFrame(data = coaches.values(), index = coaches.keys())
+
+df.to_excel('coachestest.xlsx')
+    
+
 
